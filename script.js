f4fc86b3cb45b7ffd378c6ce80884f10ee306db7
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-08-25 16:14:24 -0400

    Initial web workers configuration. Doesn't work.

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 $(function() {
-	refresh();
+	triangles = copy(preset1);
+	refresh(triangles,10);
 	render();
+	$('#t1').click(function() {
+		if (triangles.length == 2) triangles.pop();
+		else if (triangles.length == 3) { triangles.pop(); triangles.pop(); }
+	});
+	$('#t2').click(function() {
+		if (triangles.length == 1) triangles.push([[150,150],[0,75],[75,0]]);
+		else if (triangles.length == 3) triangles.pop();
+	});
+	$('#t3').click(function() {
+		if (triangles.length == 1) {
+			triangles.push([[150,150],[0,75],[75,0]]);
+			triangles.push([[150,150],[75,0],[0,75]]);
+		}
+		if (triangles.length == 2) triangles.push([[150,150],[0,75],[75,0]]);
+	});
 });
 
-
+function copy(triangles) {
+	tns = [];
+	for (var t = 0; t < triangles.length; t++) {
+		tns[t] = [];
+		for (var p = 0; p < triangles[t].length; p++) {
+			tns[t][p] = [triangles[t][p][0],triangles[t][p][1]];
+		}
+	}
+	return tns;
+}
 /* dragging */
-var triangles = [[[150,150],[0,75],[-75,0]],
+var preset1 = [[[150,150],[0,75],[-75,0]],
 			 [[187.5,225],[-37.5,0],[0,-37.5]]];
+var triangles = [[[150,150],[0,75],[75,0]]];
 
 function render() {
 	var c = $('#c3'),
 		cvs = document.getElementById('c3'),
 		context = cvs.getContext('2d'),
-		colors = [["f00","f88"],["0f0","8f8"],["66f","aaf"],["ff0","ff8"],["0ff","8ff"]],
+		colors = [["f00","f88"],["0f0","8f8"],["66f","aaf"]],
 		selectedTriangle = null,
 		selectedPoint = null,
 		canMove = false,
@@ -93,8 +119,8 @@ function render() {
 			}
 		}
 	}
-	c.mouseup(function() { canMove = false; refresh(); });
-	c.mouseleave(function() { canMove = false } );
+	c.mouseup(function() { canMove = false; refresh(triangles,10); });
+	c.mouseleave(function() { canMove = false; refresh(triangles,10); } );
 	function leave(e) {
 		selectedTriangle = null;
 		c.mousemove(null);
@@ -131,64 +157,23 @@ function drawColors() {
 		ctx.fillRect(i,0,1,10);
 	}
 }
-function refresh() {
+function refresh(triangles,quality) {
 	var canvas = document.getElementById('c'),
 		ctx = canvas.getContext('2d'),
 		canvasData = ctx.createImageData(canvas.width, canvas.height),
 		cd = canvasData.data;
 
-	function funcsFromTriangles(ts) {
-		function scale(n) {	return (n) / 75; }
-		var funcs = [];
-		for (var i = 0; i < ts.length; i++) {
-			var t = ts[i];
-			funcs.push(function(x,y,t) {
-				return [scale(t[0][0]-150) + scale(t[1][0])*x + scale(t[2][0])*y,
-						scale(t[0][1]-150) + scale(t[1][1])*x + scale(t[2][1])*y];
-			});
-		}
-		return funcs;
+	console.log("making worker");
+	var worker = new Worker('worker.js');
+	worker.onmessage = function(e) {
+		console.log("received worker message");
+		drawFractal(e.data);
 	}
+	console.log("starting worker");
+	worker.postMessage(triangles,quality,canvas.width,canvas.height);
+	console.log("started worker");
 
-	function draw() {
-		var x = Math.random()*2-1,
-			y = Math.random()*2-1,
-			c = Math.random(),
-			sierpFuncs = [ function(x,y) { return [x/2,y/2] },
-					  function(x,y) { return [(x+1)/2,y/2] },
-					  function(x,y) { return [x/2,(y+1)/2] }]
-			funcs2 = [ function(x,y) { return [-1*y,x] },
-					  function(x,y) { return [(-0.5*x)+0.5,(-0.5*y)+1] } ]
-			funcs3 = [ function(x,y) { return [.7*x - .85*y + .25, .3*x + .66*y - .44] },
-					  function(x,y) { return [-.3*x + .7*y - .31, -.67*x + .14*y + .13] } ],
-			funcs = funcsFromTriangles(triangles),
-			fColors = [0,0.5,0.99],
-			grid = [],
-			quality = 10;
-		fs = funcsFromTriangles(triangles);
-
-		for (var xR = 0; xR < canvas.width; xR++) {
-			grid[xR] = [];
-			for (var yR = 0; yR < canvas.height; yR++) {
-				grid[xR][yR] = [0,null] // freq, color
-			}
-		}
-
-		for (var n = 0; n < canvas.width * canvas.height * quality; n++) {
-			var i = Math.floor(Math.random()*funcs.length),
-				v = funcs[i](x,y,triangles[i]), x = v[0], y = v[1],
-				xR = Math.floor((x+1)/2*130)+10,
-				yR = Math.floor((y+1)/2*130)+10;
-			if (xR > 0 && xR < canvas.width && yR > 0 && yR < canvas.height) { 
-				var	c = (c+fColors[i])/2,
-					pta = grid[xR],
-					pt = pta[yR];
-				if (n > 20) {
-					if (pt[0] === 0) { pt[0] = 1; pt[1] = c}
-					else { pt[0] += 1; pt[1] = (pt[1]+c)/2 }
-				}
-			}
-		}
+	function drawFractal(grid) {
 		var freqMax = -1;
 		for (var x = 0; x < grid.length; x++) {
 			for (var y = 0; y < grid[x].length; y++) {
@@ -213,7 +198,6 @@ function refresh() {
 		}
 		ctx.putImageData(canvasData,0,0);
 	}
-	draw();
 	drawColors();
 	return false;
 }
---
+++ worker.js
@@ -0,0 +1,54 @@
+onmessage = function(e) {
+	var triangles = e.data[0],
+		quality = e.data[1],
+		width = e.data[2],
+		height = e.data[3];
+	//var results = renderFractal(triangles,quality,width,height);
+	postMessage("yay");
+}
+
+function renderFractal(triangles,quality,width,height) {
+	var x = Math.random()*2-1,
+		y = Math.random()*2-1,
+		c = Math.random(),
+		sierpFuncs = [ function(x,y) { return [x/2,y/2] },
+				  function(x,y) { return [(x+1)/2,y/2] },
+				  function(x,y) { return [x/2,(y+1)/2] }]
+		funcs2 = [ function(x,y) { return [-1*y,x] },
+				  function(x,y) { return [(-0.5*x)+0.5,(-0.5*y)+1] } ]
+		funcs3 = [ function(x,y) { return [.7*x - .85*y + .25, .3*x + .66*y - .44] },
+				  function(x,y) { return [-.3*x + .7*y - .31, -.67*x + .14*y + .13] } ],
+		fColors = [0,0.5,0.99],
+		grid = [];
+		
+	function func(x,y,t) {
+		function scale(n) {	return (n) / 75; }
+		return [scale(t[0][0]-150) + scale(t[1][0])*x + scale(t[2][0])*y,
+				scale(t[0][1]-150) + scale(t[1][1])*x + scale(t[2][1])*y];
+	};
+
+	for (var xR = 0; xR < width; xR++) {
+		grid[xR] = [];
+		for (var yR = 0; yR < height; yR++) {
+			grid[xR][yR] = [0,null] // freq, color
+		}
+	}
+
+	for (var n = 0; n < width * height * quality; n++) {
+		var i = Math.floor(Math.random()*triangles.length),
+			v = func(x,y,triangles[i]), x = v[0], y = v[1],
+			xR = Math.floor((x+1)/2*130)+10,
+			yR = Math.floor((y+1)/2*130)+10;
+		if (xR > 0 && xR < width && yR > 0 && yR < height) { 
+			var	c = (c+fColors[i])/2,
+				pta = grid[xR],
+				pt = pta[yR];
+			if (n > 20) {
+				if (pt[0] === 0) { pt[0] = 1; pt[1] = c}
+				else { pt[0] += 1; pt[1] = (pt[1]+c)/2 }
+			}
+		}
+	}
+	return grid;
+}
+
